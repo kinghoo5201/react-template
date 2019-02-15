85639cf22427ccc66704c73df5df2aa70a619300
wb-hjh418165 <970489393@qq.com>
2019-02-15 11:17:11 +0800

    fix

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -10,31 +10,29 @@ import { connectModel } from '../util';
     })
 )
 export default class App extends React.Component<any, any>{
-    render() {
+    public render() {
         console.log(this.props);
         return (
             <div>
-                <pre>
                     use browser router & it can use file's dir path to site path;
-                    root path from ./src/page
-
-                    ./
-                    --./src
-                    ----./src/page
-                    ------./src/page/index.tsx
-                    ------./src/page/404.tsx
-                    --------./src/page/test
-                    ----------./src/page/test/index.tsx
-
-                    page dir is ./src/page
-
-                    indexPage :localhost:3000=>./src/page/index.tsx
-
-                    test:localhost:3000/test=>./src/page/test/index.tsx
-
-                    if there has 404.tsx,
+                    root path from ./src/page<br />
+                    <br />
+                    ./<br />
+                    --./src<br />
+                    ----./src/page<br />
+                    ------./src/page/index.tsx<br />
+                    ------./src/page/404.tsx<br />
+                    --------./src/page/test<br />
+                    ----------./src/page/test/index.tsx<br />
+                    <br />
+                    page dir is ./src/page<br />
+                    <br />
+                    indexPage :localhost:3000=>./src/page/index.tsx<br />
+                    <br />
+                    test:localhost:3000/test=>./src/page/test/index.tsx<br />
+                    <br />
+                    if there has 404.tsx,<br />
                     the 404.tsx will redirect to ./src/page/404.tsx
-                </pre>
             </div>
         );
     }
