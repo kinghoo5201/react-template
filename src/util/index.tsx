5a422a0af7685907a191cf6ca18ef64a9651d966
wb-hjh418165 <970489393@qq.com>
2019-02-15 12:49:56 +0800

    feat:package fetch

--- src/util/index.tsx
+++ src/util/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import { init } from "@rematch/core";
 import * as _ from "lodash";
+import { resolve } from "dns";
+
+export const isDev = window.location.hostname === 'localhost';
 
 /**
  * @description:用以connect组件和model的装饰器函数
@@ -56,3 +59,114 @@ export const store: any = (() => {
 
   return store;
 })();
+
+export interface IParam {
+  url: string;
+  method?: string;
+  body?: any;
+  header?: any;
+}
+
+export interface IRes {
+  flag: boolean;
+  data: any;
+}
+type IResolveRes = (res: IRes) => void;
+export const baseAjax = (cfg: string | IParam) => {
+  return new Promise((resolve: IResolveRes) => {
+    let opt: IParam = {
+      url: '',
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      // body: '{}'
+    };
+    if (_.isString(cfg)) {
+      opt.url = cfg;
+    } else {
+      opt = {
+        ...opt,
+        ...cfg,
+      }
+    }
+    if (!_.isString(opt.body)) {
+      opt.body = JSON.stringify(opt.body);
+    }
+    return fetch(opt.url, {
+      method: opt.method,
+      headers: opt.header,
+      body: opt.body
+    })
+      .then((res: any) => {
+        if (res.status >= 400) {
+          window.console.warn('net error::', res.status);
+          return resolve({
+            flag: false,
+            data: null,
+          });
+        }
+        return res.json();
+      })
+      .then((json: any) => {
+        if (!_.isNil(json.flag) && !_.isNil(json.data)) {
+          return resolve(json);
+        }
+        return resolve({
+          flag: true,
+          data: json
+        })
+      })
+      .catch((e: any) => {
+        window.console.warn('error::', e);
+        return resolve({
+          flag: false,
+          data: null,
+        })
+      })
+  });
+}
+
+export const get = (cfg: string | IParam) => {
+  return new Promise((resolve: IResolveRes) => {
+    let url: string = _.isString(cfg) ? cfg : cfg.url;
+    if (!_.isString(cfg) && cfg.body) {
+      let tempPayload: any = {};
+      if (_.isString(cfg.body)) {
+        tempPayload = JSON.stringify(cfg.body);
+      } else {
+        tempPayload = cfg.body;
+      }
+      const query: string[] = [];
+      Object.keys(tempPayload).forEach((k: string) => {
+        query.push(`${k}=${tempPayload[k]}`);
+      });
+      const flag: boolean = /\?/.test(url);
+      url = `${url}${flag ? '&' : '?'}${query.join('&')}`;
+    }
+    const opt: IParam = {
+      url,
+      ...(_.isString(cfg) ? {} : cfg),
+      method: 'GET',
+    };
+    delete opt.body;
+    return baseAjax(opt).then((res: IRes) => {
+      return resolve(res);
+    })
+  });
+}
+
+export const post = (cfg: string | IParam) => {
+  return new Promise((resolve: IResolveRes) => {
+    let url: string = _.isString(cfg) ? cfg : cfg.url;
+    const opt: IParam = {
+      url,
+      body: '{}',
+      ...(_.isString(cfg) ? {} : cfg),
+      method: 'POST',
+    };
+    return baseAjax(opt).then((res: IRes) => {
+      return resolve(res);
+    })
+  });
+}
